a9772f40d1b2340019c6f4e24cd64c1d9879c0e1
pfundbo81 <pfund.bo@gmail.com>
2025-11-20 16:27:04 +0100

    Add files via upload

---
+++ keykicks-data.js
@@ -0,0 +1,23 @@
+{
+  "sneaker": [
+    {
+      "name": "Keychain blue/purple",
+      "price": "4.95 €",
+      "image": "https://cdn.jsdelivr.net/gh/pfundbo81/products.json/DSC_0272.JPG"
+    }
+  ],
+  "comic": [
+    {
+      "name": "Comic 1",
+      "price": "12.99 €",
+      "image": "icons/comic1.png"
+    }
+  ],
+  "other": [
+    {
+      "name": "Other 1",
+      "price": "5.99 €",
+      "image": "icons/other1.png"
+    }
+  ]
+}
